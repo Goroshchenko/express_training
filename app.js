83632682590063501577d16f452b7f57b4b772f7
Goroshchenko <mixael@yandex.ru>
2022-06-24 20:42:37 +0300

    w/o deployment

--- app.js
+++ app.js
@@ -5,22 +5,37 @@ var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 var http = require('http');
 
-var Admin = require('./controllers/Admin');
+var   Admin = require('./controllers/Admin');
+var   Home = require('./controllers/Home');
+var   Blog = require('./controllers/Blog');
+var   Page = require('./controllers/Page');
+var session = require('express-session');
 
 var app = express();
+
+
+var MongoClient = require('mongodb').MongoClient;
 
 var config = require('./config')();
 
 app.set('views', __dirname + '/templates');
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('fast-delivery-site'));
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.set('view engine', 'hjs');
+
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'hjs');
+
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
 
@@ -34,6 +49,24 @@ MongoClient.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port +
         };
         app.all('/admin*', attachDB, function(req, res, next) {
             Admin.run(req, res, next);
+        });
+        app.all('/', attachDB, function(req, res, next) {
+            Home.run(req, res, next);
+        });
+        app.all('/blog/:id', attachDB, function(req, res, next) {
+            Blog.runArticle(req, res, next);
+        });
+        app.all('/blog', attachDB, function(req, res, next) {
+            Blog.run(req, res, next);
+        });
+        app.all('/services', attachDB, function(req, res, next) {
+            Page.run('services', req, res, next);
+        });
+        app.all('/careers', attachDB, function(req, res, next) {
+            Page.run('careers', req, res, next);
+        });
+        app.all('/contacts', attachDB, function(req, res, next) {
+            Page.run('contacts', req, res, next);
         });
         http.createServer(app).listen(config.port, function(){
             console.log(
